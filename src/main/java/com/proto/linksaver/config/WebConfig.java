506b37f62d879966d40406d77c0fc01db38d1fcc
Taha Bozdemir <taha@bozdemir.net>
2024-07-08 12:05:30 +0300

    add CORS config

---
+++ src/main/java/com/proto/linksaver/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.proto.linksaver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH", "HEAD")
+                .allowedHeaders("*")
+                .exposedHeaders("Authorization", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials");
+    }
+}
